d872459af2d339496c1ae563ab9b8c870996c325
Millena Kappaun <kappaunmillena@gmail.com>
2025-03-24 22:03:35 -0300

    atividade de data e ano

---
+++ primeiro-exercicio-24-03-25/script.js
@@ -0,0 +1,7 @@
+"use strict"
+function somar(){
+    let number = document.getElementById("number").value;
+    let ano = 2025;
+    let idade = ano-number;
+    alert(`Sua idade é ${idade} anos!`);
+}
